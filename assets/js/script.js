bf4a4370661d8a325c3945151f65fe8f6f22d90d
iemeka <mekstines@gmail.com>
2021-08-07 12:13:36 +0100

    lighmode -max

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const modeSwitch = document.querySelector(".display-mode");
 
 const handleClick = (e) => {
-  document.documentElement.classList.toggle("lightmode");
+  document.documentElement.classList.toggle("lightmode-max");
 };
 
 modeSwitch.addEventListener("click", handleClick);
