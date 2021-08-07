242ff91a8c5f4b7f6fdd1bf3eb437e91868a877c
iemeka <mekstines@gmail.com>
2021-08-07 13:18:16 +0100

    3 modes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 const modeSwitch = document.querySelector(".display-mode");
-
+const states = ["lightmode-max", null, "lightmode", null];
+let mode = 1;
 const handleClick = (e) => {
-  document.documentElement.classList.toggle("lightmode-max");
+  document.documentElement.classList.remove(states[mode]);
+  mode = (mode + 1) % states.length;
+  document.documentElement.classList.add(states[mode]);
 };
 
 modeSwitch.addEventListener("click", handleClick);
